7df09702801374ad2af5e9bfc6d33dd81a308bab
senbagaraman04 <senbagaraman04@gmail.com>
2024-09-08 18:43:45 +0530

    fix(code-quality): added lint and prefix checking

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  selector: 'rams-sms-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
